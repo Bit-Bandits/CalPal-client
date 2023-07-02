613d43996b4627a64a26a0e65d1d4128733de06a
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-07-01 22:10:03 -0500

    fixing a connection

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import './App.css';
-
+import Login from './Components/Login';
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
+  //const [currentForm, setCurrentForm] = useState('login');
 
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
+  //const toggleForm = (formName) => {
+   // setCurrentForm(formName);
+  //}
 
   return (
     <div className="App">
-      <LoginForm />
+      <Login />
     </div>
   );
 }
