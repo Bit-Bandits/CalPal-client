c4d380406750e801d52667d5095b953c34463f8c
Jose Segura <jossegura2023@u.northwestern.edu>
2023-07-02 16:57:57 -0500

    added starter code for Api fetch call in Calories.js

--- src/pages/Calories.js
+++ src/pages/Calories.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+const fetch = require('node-fetch')
 
 function Calories () {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch('https://api.edamam.com/api/food-database/v2/parser?ingr=red%20apple&app_id=314fbe88&app_key=25dfafdd8b307eb5f55d06bca92f4d08')
+        .then(response => response.json())
+        .then(data => {
+            setData(data)
+        })
+        .catch(error => console.error('Error:', error));
+    }, []);
+
     return (
         <div>
-            
+            {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
         </div>
     );
 };
