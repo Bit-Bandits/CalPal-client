a713d937f8565b8f4f20a80b2ebe3e940a9cb765
MattWagner28 <matthewwagner@outlook.com>
2023-07-04 16:25:42 -0400

    got API call mapped and rendered to the page with save buttons

--- src/pages/Calories.js
+++ src/pages/Calories.js
@@ -1,45 +1,60 @@
 import React, { useEffect, useState } from 'react'; // Imports React libraries as well as useEffect and useState
 
-function Calories () {
-    const [data, setData] = useState(null);
+function Calories() {
+    const [foodData, getFoodData] = useState([]);
     const [food, setFood] = useState('');
 
-    // targets the value(food) in which the user is looking for
-    const handleFoodChange = (event) => {
-        setFood(event.target.value);
-    };
+        // targets the value(food) in which the user is looking for
+        const handleFoodChange = (event) => {
+            setFood(event.target.value);
+        };
+    
+        // handles the submit in our search to fetch data from api
+        const handleFormSubmit = (event) => {
+            event.preventDefault();
+            fetchFoodData();
+        };
 
-    // handles the submit in our search to fetch data from api
-    const handleFormSubmit = (event) => {
-        event.preventDefault();
-        fetchData();
-    };
+        useEffect(() => {
+        }, [food]);
+        
+        
 
-    // updated fetch call 
-    const fetchData = () => {
-        fetch(`https://api.edamam.com/api/food-database/v2/parser?ingr=${encodeURIComponent(food)}&app_id=314fbe88&app_key=25dfafdd8b307eb5f55d06bca92f4d08`)
-        .then(response => response.json())
-        .then(data => {
-            setData(data)
-            console.log(data)
-            const foodName = data.text;
-            const kCal = data.parsed[0].food.nutrients.ENERC_KCAL;
-            console.log('SEARCHED FOOD:', foodName);
-            console.log('CALORIES:', kCal);
-        })
-        .catch(error => console.error('Error:', error));
-    };
+    const fetchFoodData = async () => {
+        const apiUrl = `https://api.edamam.com/api/food-database/v2/parser?ingr=${encodeURIComponent(food)}&app_id=314fbe88&app_key=25dfafdd8b307eb5f55d06bca92f4d08`
+        try {
+            const response = await fetch(apiUrl)
+            const data = await response.json()
+            console.log('APICall:', data.hints);
+            getFoodData(data.hints)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    console.table(foodData.hints)
+    const list = foodData.map(food => {
+        return (
+            <li key={food.food.foodId}>
+                {food.food.label} | <button type="submit">Save</button>
+            </li>
 
+        )
+    })
     return (
         <div>
-            <form onSubmit={handleFormSubmit}>
-                <label htmlFor='food-search'>Search for a food:</label>
-                <input id="food-search" type='text' value={food} onChange={handleFoodChange} />
-                <button type='submit'>Search</button>
-            </form>
-            {data && <pre>{JSON.stringify(data.parsed[0].food.nutrients.ENERC_KCAL, null, 2)}</pre>}
-        </div>
-    );
-};
+        <form onSubmit={handleFormSubmit}>
+            <label htmlFor='food-search'>Search for a food:</label>
+            <input id="food-search" type='text' value={food} onChange={handleFoodChange} />
+            <button type='submit'>Search</button>
+        </form>
+        <ul>
+            {list}
+        </ul>
+    </div>
+    )
+
+}
+
+
 
 export default Calories;
