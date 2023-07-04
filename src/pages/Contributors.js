8ac9e605598e160c4dd62cdb65b8eb6c00651d9d
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-07-04 13:27:38 -0500

    fixing the localhost main page instead of dashboard we have now login as a 1st page on the app

--- src/pages/Contributors.js
+++ src/pages/Contributors.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { UserOutlined } from '@ant-design/icons';
 import { Avatar, Space } from 'antd';
 
-function Contributors  () {
+function Contributors() {
     const myAvatar = [
         {
             avatarUrl: "https://media.licdn.com/dms/image/D5635AQHdcuXuyL_CCg/profile-framedphoto-shrink_400_400/0/1685838029962?e=1688857200&v=beta&t=XT5hfGfZyGahVa-sEuUyoH7H1PVPLuME1GhFFUkK0jQ ",
@@ -18,25 +18,25 @@ function Contributors  () {
     ]
     return (
         <>
-        {myAvatar.map((avatars) => {
-            return (
-                <Space direction="vertical" size={16}>
-                <Space wrap size={16}>
-                  <Avatar size={75} icon={avatars.avatarUrl} />
-                   
-                    
-            
-                </Space>
-                <span>{avatars.text}</span>
-    
-                <button>{avatars.linkedin}</button>
-        
-  
+            {myAvatar.map((avatars) => {
+                return (
+                    <Space direction="vertical" size={16}>
+                        <Space wrap size={16}>
+                            <Avatar size={75} icon={avatars.avatarUrl} />
 
-            </Space>
-            )
-        })}
-            
+
+
+                        </Space>
+                        <span>{avatars.text}</span>
+
+                        <button>{avatars.linkedin}</button>
+
+
+
+                    </Space>
+                )
+            })}
+
         </>
     );
 };
