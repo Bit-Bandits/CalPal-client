7bd14807f6056feffba9fb5bd1ece172cce83905
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-07-04 10:43:04 -0500

    fixing overwritten code

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -12,22 +12,23 @@ export const Register = (props) => {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ query: `mutation { addUser(email: "${email}", password: "${pass}", username: "${name}") { token } }` })
-    } ) .then(res => res.json()).then(data => {console.log(data)})}
+        }).then(res => res.json()).then(data => { console.log(data) })
+    }
 
     return (
         <div className="auth-form-container">
             <h2>Register</h2>
-        <form className="register-form" onSubmit={handleSubmit}>
-            <label htmlFor="name">Full name</label>
-            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
-            <label htmlFor="email">email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-            <label htmlFor="password">password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <button type="submit">Sign up</button>
-        </form>
-        <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
-    </div>
+            <form className="register-form" onSubmit={handleSubmit}>
+                <label htmlFor="name">Full name</label>
+                <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
+                <label htmlFor="email">email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <label htmlFor="password">password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <button type="submit">Sign up</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+        </div>
     )
 }
 
