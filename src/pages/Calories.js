3914bdb66b61cb8cc80ef739d84b2146f69ef60e
Jose Segura <jossegura2023@u.northwestern.edu>
2023-07-05 21:22:45 -0500

    food calculated by serving size

--- src/pages/Calories.js
+++ src/pages/Calories.js
@@ -27,50 +27,43 @@ function Calories() {
             const response = await fetch(apiUrl)
             const data = await response.json()
             console.log('APICall:', data.hints);
-            setFoodData(data.hints.map(item => ({ ...item, quantity: 1, unit: 'gram', servingSize: 1 })))
+            setFoodData(data.hints.map(item => ({ ...item, servings: 1 , unit: 'gram' })))
         } catch (e) {
             console.log(e)
         }
     }
 
-    const handleQuantityChange = (index, value) => {
-        setFoodData(foodData.map((item, i) => i === index ? { ...item, quantity: value} : item))
+    const handleServingsChange = (index, value) => {
+        setFoodData(foodData.map((item, i) => i === index ? { ...item, servings: value} : item))
     }
 
     const handleUnitChange = (index, value) => {
         setFoodData(foodData.map((item, i) => i === index ? { ...item, unit: value} : item))
     }
 
-    const handleServingSizeChange = (index, value) => {
-        setFoodData(foodData.map((item, i) => i === index ? { ...item, servingSize: value } : item))
-    }
-
-    const handleSaveFood = (calories, quantity, servingSize) => {
-        setTotalCalories(totalCalories + (calories * quantity * servingSize));
+    const handleSaveFood = (calories, servings) => {
+        setTotalCalories(totalCalories + (calories * servings));
     }
 
     // console.table(foodData.hints)
     const list = foodData.map((food, index) => {
-        const calories = (food.food.nutrients.ENERC_KCAL * food.quantity * food.servingSize).toFixed(2);
+        const calories = (food.food.nutrients.ENERC_KCAL * food.servings).toFixed(2);
         return (
             <li key={food.food.foodId}>
                 {food.food.label} | Calories: {calories}
                 <input 
-                    type="number" value={food.quantity} onChange={(e) => handleQuantityChange(index, e.target.value)} 
+                    type="number" 
+                    value={food.servings} 
+                    onChange={(e) => handleServingsChange(index, e.target.value)} 
                     min='1' 
                 />
                 <select value={food.unit} onChange={(e) => handleUnitChange(index, e.target.value)}>
                     <option value="gram">Gram</option>
                     <option value="lb">Pound</option>
+                    <option value="oz">Ounce</option>
+                    <option value="fl oz">Fl Oz</option>
                 </select>
-                <input
-                    type="number"
-                    value={food.servingSize}
-                    onChange={(e) => handleServingSizeChange(index, e.target.value)}
-                    min="0.01"
-                    step="0.01"
-                />
-                <button onClick={() => handleSaveFood(food.food.nutrients.ENERC_KCAL, food.quantity, food.servingSize)}>Save</button>
+                <button onClick={() => handleSaveFood(food.food.nutrients.ENERC_KCAL, food.servings)}>Save</button>
             </li>
 
         )
