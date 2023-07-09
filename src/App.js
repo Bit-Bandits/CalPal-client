661e4881a83b8185145312d02a6783bc6eaf1a14
Mohamed <114217025+melhelow@users.noreply.github.com>
2023-07-08 19:01:31 -0500

    uncomment the graphql, and comment in the heroku connection

--- src/App.js
+++ src/App.js
@@ -17,13 +17,13 @@ import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
 import Homepage from "./pages/Homepage";
 
 
-const httpLink = createHttpLink({
-  uri: 'https://cal-pal-server-273e253c14e5.herokuapp.com/graphql'
-});
-
 // const httpLink = createHttpLink({
-//   uri: '/graphql'
+//   uri: 'https://cal-pal-server-273e253c14e5.herokuapp.com/graphql'
 // });
+
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
 
 
 const authLink = setContext((_, { headers }) => {
