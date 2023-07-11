61bc12aa704530efe891fe0fde907439b8a7b420
Mohamed <114217025+melhelow@users.noreply.github.com>
2023-07-11 13:33:24 -0500

    removed showModal

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,46 +1,43 @@
-import React, { useState } from 'react';
-import { Layout, Menu, Button } from 'antd';
+import React from 'react';
+import { Layout, Menu } from 'antd';
 import { Nav } from 'react-bootstrap';
 
-
-import Auth from '../utils/auth'; // Import the Auth variable
+import Auth from '../utils/auth';
 import { Link } from 'react-router-dom';
 const { Header } = Layout;
 
 const NavBar = () => {
-  const [showModal, setShowModal] = useState(false);
+  const handleLogoutClick = () => {
+    Auth.logout();
+  };
 
   return (
     <Layout className="layout">
-      <Header style={{ display: 'flex', justifyContent: 'center', }}>
+      <Header style={{ display: 'flex', justifyContent: 'center' }}>
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
           <Menu.Item key="1">
-          <Link to="/">Home</Link>
-            </Menu.Item>
+            <Link to="/">Home</Link>
+          </Menu.Item>
           <Menu.Item key="2">
-          <Link to="/dashboard">Dashboard</Link>
+            <Link to="/dashboard">Dashboard</Link>
           </Menu.Item>
           <Menu.Item key="3">
             <Link to="/calories">Count my Calories</Link>
           </Menu.Item>
           <Menu.Item key="4">
-          <Link to="/about-us">About Us</Link>
+            <Link to="/about-us">About Us</Link>
           </Menu.Item>
         </Menu>
         <div>
-          {/* <Button type="primary" style={{ marginRight: '10px' }}>Logout</Button> */}
           {console.log(Auth.loggedIn())};
           {Auth.loggedIn() ? (
             <>
-
-              <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+              <Nav.Link onClick={handleLogoutClick}>Logout</Nav.Link>
             </>
           ) : (
-            <Nav.Link onClick={() => {
-              setShowModal(true)
-              window.location.pathname = '/login'
-            }
-          }>Login</Nav.Link>
+            <Nav.Link onClick={() => (window.location.pathname = '/login')}>
+              Login
+            </Nav.Link>
           )}
         </div>
       </Header>
