dd857729fa499adde08d40ae03078dff4b29a2b1
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-07-11 21:20:38 -0500

    adding a new navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
+// import { Layout, Menu } from 'antd';
 // import { Nav } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
 import { Link } from 'react-router-dom';
-const { Header } = Layout;
+// const { Header } = Layout;
 
 const Navbar = () => {
   const handleLogoutClick = () => {
@@ -12,36 +12,74 @@ const Navbar = () => {
   };
 
   return (
-    <Layout className="layout">
-      <Header style={{ display: 'flex', justifyContent: 'center' }}>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1">
-            <Link to="/">Home</Link>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Link to="/dashboard">Dashboard</Link>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Link to="/calories">Count my Calories</Link>
-          </Menu.Item>
-          <Menu.Item key="4">
-            <Link to="/about-us">About Us</Link>
-          </Menu.Item>
-        </Menu>
+    <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" /></svg>
+        <span class="font-semibold text-xl tracking-tight">Calpal</span>
+      </div>
+      <div class="block lg:hidden">
+        <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+        </button>
+      </div>
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="text-sm lg:flex-grow">
+          <a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            Home
+          </a>
+          <a href="/dashboard" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            Dashboard
+          </a>
+          <a href="/calories" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            Count my Calories
+          </a>
+          <a href="/about-us" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+            About Us
+          </a>
+        </div>
         <div>
-          {console.log(Auth.loggedIn())};
           {Auth.loggedIn() ? (
             <>
-              <Link onClick={handleLogoutClick}>Logout</Link>
+              <a href="#" onClick={handleLogoutClick} class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Logout</a>
             </>
           ) : (
-            <Link onClick={() => (window.location.pathname = '/login')}>
-              Login
-            </Link>
+            <a href="/login" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Login</a>
           )}
+
+
         </div>
-      </Header>
-    </Layout>
+      </div>
+    </nav>
+    // <Layout className="layout">
+    //   <Header style={{ display: 'flex', justifyContent: 'center' }}>
+    //     <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+    //       <Menu.Item key="1">
+    //         <Link to="/">Home</Link>
+    //       </Menu.Item>
+    //       <Menu.Item key="2">
+    //         <Link to="/dashboard">Dashboard</Link>
+    //       </Menu.Item>
+    //       <Menu.Item key="3">
+    //         <Link to="/calories">Count my Calories</Link>
+    //       </Menu.Item>
+    //       <Menu.Item key="4">
+    //         <Link to="/about-us">About Us</Link>
+    //       </Menu.Item>
+    //     </Menu>
+    //     <div>
+    //       {console.log(Auth.loggedIn())};
+    // {Auth.loggedIn() ? (
+    //   <>
+    //     <Link onClick={handleLogoutClick}>Logout</Link>
+    //   </>
+    // ) : (
+    //   <Link onClick={() => (window.location.pathname = '/login')}>
+    //     Login
+    //   </Link>
+    // )}
+    //     </div>
+    //   </Header>
+    // </Layout>
   );
 };
 
