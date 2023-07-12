8e9317a8bfe60b98de2ab982e026ac9cb0d71443
Mohamed <114217025+melhelow@users.noreply.github.com>
2023-07-12 17:22:17 -0500

    CSS and added logo rmv hidden tab

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import svgLogo from '../assets/logo.png';
 // import { Layout, Menu } from 'antd';
 // import { Nav } from 'react-bootstrap';
 
@@ -10,20 +11,27 @@ const Navbar = () => {
   const handleLogoutClick = () => {
     Auth.logout();
   };
+  const logo = [
+    {
+      src : svgLogo,
+      
+
+    }
+  ]
 
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+    <nav className="flex justify-center text-center bg-[#35CBD2] py-0">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" /></svg>
-        <span className="font-semibold text-xl tracking-tight">Calpal</span>
+       <img src={logo[0].src} alt="logo" className="h-40 w-15 mr-3" />
+        <span className="font-semibold text-3xl tracking-tight">Calpal</span>
       </div>
-      <div className="block lg:hidden">
+      {/* <div className="block lg:hidden">
         <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
           <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
         </button>
-      </div>
-      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div className="text-sm lg:flex-grow">
+      </div> */}
+      <div className="w-full  flex-grow lg:flex lg:items-center lg:w-auto  ">
+        <div className=" flex text-lg:flex-grow ">
           <a href="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
             Home
           </a>
@@ -37,10 +45,10 @@ const Navbar = () => {
             About Us
           </a>
         </div>
-        <div>
+        <div className='flex p-12'>
           {Auth.loggedIn() ? (
             <>
-              <a href="www.example.com" onClick={handleLogoutClick} className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Logout</a>
+              <a href="/" onClick={handleLogoutClick} className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Logout</a>
             </>
           ) : (
             <a href="/login" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Login</a>
