0cfae99108efccfc2fbfc123db1454aabdf030c1
Mohamed <114217025+melhelow@users.noreply.github.com>
2023-07-12 13:39:33 -0500

    Updated .gitignore to ignore .env file

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 // import { Nav } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 // const { Header } = Layout;
 
 const Navbar = () => {
