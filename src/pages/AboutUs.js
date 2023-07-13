f607e0dd8be9bb0df02da2ea07d4279ebf5cc86f
Mohamed <114217025+melhelow@users.noreply.github.com>
2023-07-12 20:57:39 -0500

    fixed fixed about us

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -4,37 +4,37 @@ import React from 'react';
 function AboutUs() {
   let message = ``;
   return (
-    <section class="section-white">
+    <section className="section-white">
 
-      <div class="container">
+      <div className="container">
 
-        <div class="row">
+        <div className="row">
 
-          <div class="col-md-12 text-center">
+          <div className="col-md-12 text-center">
 
-            <h2 id ="about-us" class="section-title">The Team Behind Calpal</h2>
+            <h2 id ="about-us" className="section-title">The Team Behind Calpal</h2>
 
-            <p class="section-subtitle">{message}</p>
+            <p className="section-subtitle">{message}</p>
 
           </div>
           
-          <div class="col-sm-6 col-md-4">
+          <div className="col-sm-6 col-md-4">
 
-<div class="team-item">
+<div className="team-item">
 
-  <img src="https://www.linkpicture.com/q/345454302_249964134275081_1706394919925166623_n.jpeg" class="team-img" alt="pic" />
+  <img src="https://www.linkpicture.com/q/345454302_249964134275081_1706394919925166623_n.jpeg" className="team-img" alt="pic" />
   <h3>Goldie Rizova</h3>
-  <div class="team-info"><p>Software Engineer</p></div>
+  <div className="team-info"><p>Software Engineer</p></div>
   <p>I am a Software Engineer currently in a bootcamp.</p>
 
-  <ul class="team-icon">
+  <ul className="team-icon">
 
-    <li><a href="https://github.com/Goldie369" class="GitHub">
-      <i class="fa fa-Github"></i>
+    <li><a href="https://github.com/Goldie369" className="GitHub">
+      <i className="fa fa-Github"></i>
     </a></li>
 
-    <li><a href="https://www.linkedin.com/in/zlatelina-rizova-6b7487266/" class="LinkedIn">
-      <i class="fa fa-LinkedIn"></i>
+    <li><a href="https://www.linkedin.com/in/zlatelina-rizova-6b7487266/" className="LinkedIn">
+      <i className="fa fa-LinkedIn"></i>
     </a></li>
 
 
@@ -43,23 +43,23 @@ function AboutUs() {
 
 </div>
 </div>
-<div class="col-sm-6 col-md-4">
+<div className="col-sm-6 col-md-4">
 
-<div class="team-item">
+<div className="team-item">
 
-  <img src="https://www.linkpicture.com/q/me_3.jpg" class="team-img" alt="pic" />
+  <img src="https://www.linkpicture.com/q/me_3.jpg" className="team-img" alt="pic" />
   <h3>Mohamed Elhelw</h3>
-  <div class="team-info"><p>Software Engineer</p></div>
+  <div className="team-info"><p>Software Engineer</p></div>
   <p>Full stack web developer.</p>
 
-  <ul class="team-icon">
+  <ul className="team-icon">
 
-    <li><a href="https://github.com/melhelow" class="GitHub">
-      <i class="fa fa-Github"></i>
+    <li><a href="https://github.com/melhelow" className="GitHub">
+      <i className="fa fa-Github"></i>
     </a></li>
 
-    <li><a href="https://www.linkedin.com/in/mohamed-elhelw-76a888271/" class="LinkedIn">
-      <i class="fa fa-LinkedIn"></i>
+    <li><a href="https://www.linkedin.com/in/mohamed-elhelw-76a888271/" className="LinkedIn">
+      <i className="fa fa-LinkedIn"></i>
     </a></li>
 
  
@@ -70,23 +70,23 @@ function AboutUs() {
 </div>
 </div>
 
-<div class="col-sm-6 col-md-4">
+<div className="col-sm-6 col-md-4">
 
-<div class="team-item">
+<div className="team-item">
 
-  <img src="https://www.linkpicture.com/q/IMG_9280_1.jpg" class="team-img" alt="pic" />
+  <img src="https://www.linkpicture.com/q/IMG_9280_1.jpg" className="team-img" alt="pic" />
 
   <h3>Matt Wagner</h3>
 
-  <div class="team-info"><p>Junior Engineer</p></div>
+  <div className="team-info"><p>Junior Engineer</p></div>
 
   <p>About me</p>
 
-  <ul class="team-icon">
+  <ul className="team-icon">
 
-    <li><a href="https://github.com/orgs/Bit-Bandits/people/mattwagner28" class="Github"><i class="fa fa-Github"></i></a></li>
+    <li><a href="https://github.com/orgs/Bit-Bandits/people/mattwagner28" className="Github"><i className="fa fa-Github"></i></a></li>
 
-    <li><a href="https://www.linkedin.com/in/matthewtwagner" class="LinkedIn"><i class="fa fa-LinkedIn"></i></a></li>
+    <li><a href="https://www.linkedin.com/in/matthewtwagner" className="LinkedIn"><i className="fa fa-LinkedIn"></i></a></li>
 
 
 
@@ -96,23 +96,23 @@ function AboutUs() {
 
 </div>
 
-<div class="col-sm-6 col-md-4">
+<div className="col-sm-6 col-md-4">
 
-<div class="team-item">
+<div className="team-item">
 
-  <img src="https://www.linkpicture.com/q/hey-me.jpg" class="team-img" alt="pic" />
+  <img src="https://www.linkpicture.com/q/hey-me.jpg" className="team-img" alt="pic" />
 
   <h3>José Segura</h3>
 
-  <div class="team-info"><p>Junior Engineer</p></div>
+  <div className="team-info"><p>Junior Engineer</p></div>
 
   <p> Junior Engineer</p>
 
-  <ul class="team-icon">
+  <ul className="team-icon">
 
-    <li><a href="https://github.com/Jose-seg" class="GitHub"><i class="fa fa-GitHub"></i></a></li>
+    <li><a href="https://github.com/Jose-seg" className="GitHub"><i className="fa fa-GitHub"></i></a></li>
 
-    <li><a href="https://www.linkedin.com/in/jose-segura-7a998319a" class="LinkedIn"><i class="fa fa-LinkedIn"></i></a></li>
+    <li><a href="https://www.linkedin.com/in/jose-segura-7a998319a" className="LinkedIn"><i className="fa fa-LinkedIn"></i></a></li>
 
 
 
@@ -124,23 +124,23 @@ function AboutUs() {
 </div>
 
 </div>
-<div class="col-sm-6 col-md-4">
+<div className="col-sm-6 col-md-4">
 
-<div class="team-item">
+<div className="team-item">
 
-<img src="https://www.linkpicture.com/q/AntBioPic.jpeg" class="team-img" alt="pic" />
+<img src="https://www.linkpicture.com/q/AntBioPic.jpeg" className="team-img" alt="pic" />
 
 <h3>Anthony Groce</h3>
 
-<div class="team-info"><p>Junior Engineer</p></div>
+<div className="team-info"><p>Junior Engineer</p></div>
 
 <p>About me</p>
 
-<ul class="team-icon">
+<ul className="team-icon">
 
-  <li><a href="https://github.com/AnthGrow" class="GitHub"><i class="fa fa-Github"></i></a></li>
+  <li><a href="https://github.com/AnthGrow" className="GitHub"><i className="fa fa-Github"></i></a></li>
 
-  <li><a href="www.example.com" class="LinkedIn"><i class="fa fa-LinkedIn"></i></a></li>
+  <li><a href="www.example.com" className="LinkedIn"><i className="fa fa-LinkedIn"></i></a></li>
 
  
 
